b13d8c075d9b6d5082e377ee79720875f0f9a161
DESKTOP-5L3C9JQ\User <golddesiner93@gmail.com>
2019-12-03 23:02:57 +0900

    Clicker Game 1. Project Settings - Unity Package Settings, 9 : 16 Resoulution, Main Camera fixed 2. Create Title Scene, Main Scene and Build Settings the 2 Scenes. 3. Title Scene - Background Image, Title Image, Touch To Start Text, Button UI 4. UI Image - Stretch To Stretch,

---
+++ HGS_Unity_Game_First_Project/Assets/Scripts/TitleController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// 2019.12.03 - 코드 추가
+using UnityEngine.UI;
+// 2019.12.03 - 코드 추가
+using UnityEngine.SceneManagement;
+
+// 2019.12.03 - 클래스 추가
+
+public class TitleController : MonoBehaviour
+{
+    // 2019.12.03 - 변수 추가
+    [SerializeField]
+    private Button mStartButton;
+    // 2019.12.03 - 변수 추가
+    [SerializeField]
+    private Text mStatusText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 2019.12.03 - 코드 추가
+        mStartButton.onClick.AddListener(StartMainGame);
+        // 2019.12.03 - 코드 추가
+        mStatusText.text = "Touch to Start";
+    }
+
+    // 2019.12.03 - 함수 추가
+    public void StartMainGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}

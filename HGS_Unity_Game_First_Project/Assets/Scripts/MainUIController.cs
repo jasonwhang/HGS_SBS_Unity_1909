eca8f30dc775cf244af545e0026d56dd83727c6d
DESKTOP-61OS7VJ\USER <jasonprogrammer93@gmail.com>
2019-12-04 19:49:36 +0900

    20191204 2교시 수업내용

---
+++ HGS_Unity_Game_First_Project/Assets/Scripts/MainUIController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 2019.12.04 - 클래스 추가
+
+public class MainUIController : MonoBehaviour
+{
+    // 변수가 한개여도 여러 군데에서 공유해서 사용하기 위하여 static으로 만듬
+    private static int mUIMoveHash = Animator.StringToHash("Move");
+
+    [SerializeField]
+    Animator[] mWindowAnims;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // 버튼에 연결하여 AddListener에 for문으로 index별로 넣는 것은 되지 않는다.
+    public void MoveWindow(int id)
+    {
+        mWindowAnims[id].SetTrigger(mUIMoveHash);
+    }
+}

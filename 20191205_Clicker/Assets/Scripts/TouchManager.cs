6b39562d9f67fd0068863857c33ce2b481402ffe
DESKTOP-61OS7VJ\USER <jasonprogrammer93@gmail.com>
2019-12-05 18:56:51 +0900

    20191205 1교시 수업내용 1. Background용 Canvas와 UI용 Canvas를 구분시켜주기 2. RayCast에 대한 설명 3. RayCast를 적용시키기 위하여 Background Canvas와 UI의 Canvas의 Layer를 다르게 해주기 4. 3D공간으로 변경시켜 Layer 2개의 Z좌표를 다르게 하기 5. UI용 Canvas와 Background용 Canvas 사이에 Collider 검출을 위한 3DCube를 배치하기

---
+++ 20191205_Clicker/Assets/Scripts/TouchManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray();
+    }
+}

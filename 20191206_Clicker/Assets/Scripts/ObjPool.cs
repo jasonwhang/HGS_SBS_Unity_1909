0346f3a210c89b11faa9b80efe779de46e7579e9
DESKTOP-61OS7VJ\USER <jasonprogrammer93@gmail.com>
2019-12-06 19:52:30 +0900

    20191206 2교시 수업내용

---
+++ 20191206_Clicker/Assets/Scripts/ObjPool.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjPool : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] mOriginArr;
+
+    // mOriginArr이 늘어나지는 않을 것인데 이것을 굳이 List로 하여 이중List를 사용할 필요는 없다.
+    [SerializeField]
+    private List<GameObject>[] mPool;
+
+    protected void PoolSetUp()
+    {
+        // List[]를 초기화해준다.
+        mPool = new List<GameObject>[mOriginArr.Length];
+
+        for(int i = 0; i < mPool.Length; i++)
+        {
+            mPool[i] = new List<GameObject>();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PoolSetUp();
+    }
+
+    public GameObject GetFromPool(int id)
+    {
+        // mPool[id]는 List이기 때문에 Length가 아닌 Count이다.
+        for(int i = 0; i < mPool[id].Count; i++)
+        {
+            if(!mPool[id][i].gameObject.activeInHierarchy)
+            {
+                mPool[id][i].gameObject.SetActive(true);
+                return mPool[id][i];
+            }
+        }
+
+        // 숙제
+    }
+
+    // 숙제
+}
---
+++ 20191206_Clicker/Assets/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 2019.12.06 - 클래스 추가
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    private float time;
+
+    private void OnEnable()
+    {
+        StartCoroutine(CountDown());
+    }
+
+    IEnumerator CountDown()
+    {
+        yield return new WaitForSeconds(time);
+        gameObject.SetActive(false);
+    }
+}

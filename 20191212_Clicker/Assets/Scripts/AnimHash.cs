66d1b30ab6c3510b14ef21f3ba8e9674d5f8cbdd
DESKTOP-61OS7VJ\USER <jasonprogrammer93@gmail.com>
2019-12-12 19:50:59 +0900

    20191212 목요일 - 2교시 수업내용 1. 동료캐릭터 만들기 2. 동료캐릭터 스크립트로 제어하기(Colleauge.cs) 3. AnimHash를 만들기

---
+++ 20191212_Clicker/Assets/Scripts/AnimHash.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 2019.12.12 - 크
+
+public class AnimHash
+{
+    public static readonly int move = Animator.StringToHash("IsMove");
+}
---
+++ 20191212_Clicker/Assets/Scripts/Colleague.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colleague : MonoBehaviour
+{
+    private Rigidbody2D mRb2D;
+
+    [SerializeField]
+    private float mSpeed;
+
+    private Animator mAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private IEnumerator Movement()
+    {
+        WaitForSeconds moveTime = new WaitForSeconds(1);
+
+        while(true)
+        {
+            // Set Direction
+            int dir = Random.Range(0, 2);
+            if(dir == 0)
+            {
+                transform.rotation = Quaternion.identity;
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+            }
+            // Move or Stay
+            int moveOrStay = Random.Range(0, 2);
+            if (moveOrStay == 0)
+            {
+                mRb2D.velocity = Vector2.zero;
+            }
+            else
+            {
+                mRb2D.velocity = transform.right * -mSpeed;
+            }
+            // Wait
+            yield return moveTime;
+        }
+    }
+
+    private IEnumerator Function(float time)
+    {
+        WaitForSeconds term = new WaitForSeconds(time);
+
+        while(true)
+        {
+            // wait
+            yield return term;
+            // run special function
+        }
+    }
+}

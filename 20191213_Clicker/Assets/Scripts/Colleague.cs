5f4fefcfb6ac53be22476732c8600d6cb82a1b4a
DESKTOP-61OS7VJ\USER <jasonprogrammer93@gmail.com>
2019-12-13 18:52:51 +0900

    20191213 금요일 1교시 수업내용 1. 동료캐릭터를 Prefab화 시키기 2. ColleageController.cs를 만들어 동료캐릭터를 데이터클래스로 제어하기

---
+++ 20191213_Clicker/Assets/Scripts/Colleague.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colleague : MonoBehaviour
+{
+    private Rigidbody2D mRB2D;
+    [SerializeField]
+    private float mSpeed;
+
+    // 2019.12.13 금요일 - 변수 추가
+    [SerializeField]
+    private Transform mEffectPos;
+
+    private Animator mAnim;
+
+    // 2019.12.13 금요일 - 변수 추가
+    private string mName;
+    private int mID;
+
+
+    private void Awake()
+    {
+        mRB2D = GetComponent<Rigidbody2D>();
+        mAnim = GetComponent<Animator>();
+    }
+    // 2019.12.13 금요일 - 함수 수정(Start -> Init)
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    //    // 2019.12.13 금요일 - 코드 추가
+    //    StartCoroutine(Movement());
+    //    StartCoroutine(Function(1));
+    //}
+
+    public void Init(string Name, int id, float period)
+    {
+        // 2019.12.13 금요일 - 코드 추가
+        mName = Name;
+        mID = id;
+
+        // 2019.12.13 금요일 - 코드 추가
+        StartCoroutine(Movement());
+
+        // 2019.12.13 금요일 - 코드 수정
+        //StartCoroutine(Function(1));
+        StartCoroutine(Function(period));
+    }
+
+    private IEnumerator Movement()
+    {
+        WaitForSeconds moveTime = new WaitForSeconds(1);
+        while(true)
+        {
+            int dir = Random.Range(0, 2);
+            if(dir == 0) // see left side
+            {
+                transform.rotation = Quaternion.identity;
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+            }
+
+            int moveOrStay = Random.Range(0, 2);
+            if(moveOrStay == 0)
+            {
+                mRB2D.velocity = Vector2.zero;
+                mAnim.SetBool(AnimHash.Move, false);
+            }
+            else
+            {
+                mRB2D.velocity = transform.right * -mSpeed;
+                mAnim.SetBool(AnimHash.Move, true);
+            }
+
+            yield return moveTime;
+        }
+    }
+
+    private IEnumerator Function(float time)
+    {
+        WaitForSeconds term = new WaitForSeconds(time);
+        while(true)
+        {
+            yield return term;
+            // run special function
+            // 2019.12.13 금요일 - 코드 추가
+            // Time.time은 0초부터 올라가는 플레이 시간이다.
+            // 만일 No.1동료가 행동을 했다면 No.1(0)
+            Debug.LogFormat("{0}({1}) finish job current time is {2}", mName, mID, Time.time);
+        }
+    }
+}
---
+++ 20191213_Clicker/Assets/Scripts/ColleagueController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 2019.12.13 금요일 - 클래스추가
+
+public class ColleagueController : MonoBehaviour
+{
+    private ColleagueData[] mDataArr;
+    [SerializeField]
+    private Colleague[] mPrefabArr;
+    [SerializeField]
+    private Transform mSpawnPos;
+
+    private void Awake()
+    {
+        mDataArr = new ColleagueData[3];
+
+        mDataArr[0] = new ColleagueData();
+        mDataArr[0].Name = "No.1";
+        mDataArr[0].Level = 0;
+        mDataArr[0].JobTime = 1.1f;
+        mDataArr[0].CostCurrent = 100;
+
+        mDataArr[1] = new ColleagueData();
+        mDataArr[1].Name = "No.2";
+        mDataArr[1].Level = 0;
+        mDataArr[1].JobTime = 1f;
+        mDataArr[1].CostCurrent = 200;
+
+        mDataArr[2] = new ColleagueData();
+        mDataArr[2].Name = "No.3";
+        mDataArr[2].Level = 0;
+        mDataArr[2].JobTime = 1.5f;
+        mDataArr[2].CostCurrent = 300;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // amount를 해주는 이유는 1레벨업씩만 하는 것이 아닌 10레벨업씩 할 수도 있기 때문이다.
+    // 예를들어 함수를 호출할 때 0번데이터를 10레벨업을 할 것이다.
+    public void AddLevel(int id, int amount)
+    {
+
+    }
+}
+
+public class ColleagueData
+{
+    public string Name;
+    public int Level;
+    public float JobTime;
+    public double CostCurrent;
+}

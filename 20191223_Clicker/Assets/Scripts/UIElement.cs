325aa506240a2ba13076d1db4bf3dd247f6cd48d
DESKTOP-61OS7VJ\USER <jasonprogrammer93@gmail.com>
2019-12-23 18:57:48 +0900

    20191223 월요일 - 1교시 수업내용 1. 동료캐릭터가 터뜨려야 되는 텍스트 이펙트에 대한 기능을 구현해보자. 2. 동료에 대한 아이콘을 추가해주자. 3. 플레이어가 가지고 있는 데이터나 기능을 설계해보자.

---
+++ 20191223_Clicker/Assets/Scripts/UIElement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIElement : MonoBehaviour
+{
+    [SerializeField]
+    private Image mIcon;
+    [SerializeField]
+    private Text mNameText, mLevelText, mContentsText, mPurchaseText, mCostText;
+    [SerializeField]
+    private Button mPurchaseButton;
+    private int mID;
+
+    public void Init(Sprite icon, int id, string name,
+                     string contents, string purchaseText,
+                     int level, double value, double cost, double time,
+                     AnimHash.TwoIntPramCallback callback)
+    {
+        mIcon.sprite = icon;
+        mID = id;
+        mNameText.text = name;
+        mPurchaseButton.onClick.AddListener(()=> { callback(mID, 1); });
+        Renew(contents, purchaseText, level, value, cost, time);
+    }
+    public void Renew(string contents, string purchaseText, int level, double value, double cost,double time)
+    {
+        mLevelText.text = "LV. " + level.ToString();
+        mContentsText.text = string.Format(contents, UnitBuilder.GetUnitStr(value),
+                                                     time.ToString("N1"));
+        mCostText.text = UnitBuilder.GetUnitStr(cost);
+        mPurchaseText.text = purchaseText;
+    }
+}
